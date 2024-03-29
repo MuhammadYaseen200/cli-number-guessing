0b10ba3a94fc8f252ddc5e8e252514bdab7247fe
MuhammadYaseen200 <muhammadyaseenabdulghanijumani@gmail.com>
2024-03-30 04:49:25 +0500

    Another practice programming cls -number-guessing game.

---
+++ index.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// 1) computerv will generate a random number - Done
+//const backendinputnumber = 7
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+// console.log(randomNumber);
+//2) user input for guessing a number  -Done
+const answer = await inquirer.prompt([
+    {
+        name: "UserGuessedNumber",
+        type: "number",
+        mesage: "You can choose a number between 1-10",
+    },
+]);
+//Conditional statements
+// 3) compare user input with computer generated number and show result
+if (answer.UserGuessedNumber === randomNumber) {
+    console.log("Congratulations..!! Your guessing number is correct.");
+}
+else {
+    console.log("you lose!! you guess a wrong number");
+    console.log("The correct answer is:" + randomNumber);
+}
